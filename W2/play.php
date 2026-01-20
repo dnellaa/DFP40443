8cf79830b4144dba6cff0ad0e4d1cfec7e5866e8
dnellaa <daniellabatrisya98@gmail.com>
2026-01-20 15:17:57 +0800

    first commit

---
+++ W2/play.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Daniella";
+echo "Hello world $name";
+echo ' This is a full stack class '.$name;
+echo "<h1> My name is Daniella Batrisya </h1>";
+echo "<h2> My PA is Miss Athirah </h2>";
+echo "<h3> I'm from Jabatan Teknologi Maklumat dan Komunikasi </h3>";
+
+?>
