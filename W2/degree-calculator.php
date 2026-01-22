33a1c655d7238386a6509e561c749f43ab3d128a
dnellaa <daniellabatrisya98@gmail.com>
2026-01-22 12:09:36 +0800

    Add files via upload

---
+++ W2/degree-calculator.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+</head> 
+<body>
+
+
+ <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<label>Kelvin</label>
+<input name="kelvinVal">
+<input type="submit" value="Calculate">
+<input type="submit" value="Clear">
+</form>
+
+</body>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $kelvin = $_POST['kelvinVal'];
+    $result = $kelvin- 273.15; 
+
+
+
+}
+
+?>
+
+<?php
+echo $celcius;
+?>
+
+</html>
